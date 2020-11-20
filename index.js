ae98bf51aec6b7baee11635b848410fd3cc79362
Kevin Conroy <74108431+Kevin-Conroy@users.noreply.github.com>
2020-11-20 13:37:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-$(document).ready(function(){
-   $('button').click(function() {
+function addListItem () {
+   $("#js-shopping-list-form > button").on('click', function(event) {
+     event.preventDefault()
     var mylist = $('#shopping-list-entry').val();
-    $('#shopping-list').append('<li>'+mylist+'</li>');
-     return false;
- });
+    $('.shopping-list').append(
+      ` <li>
+        <span class="shopping-item">${mylist}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li> `);
+   }); 
+}
+  $(addListItem);
+
+function deleteListItem() {
+  $("body").on('click', ".shopping-item-delete", function(event) {
+    event.preventDefault() 
+    $(this).parent().parent().remove();
+  });
+}
+$(deleteListItem);
+ 
+function checkListItem() {
+  $("body").on("click", '.shopping-item-toggle', function() {
+    $(this).closest('li').children().first().toggleClass('shopping-item__checked');
+  });
+}
+$(checkListItem);
